77ff15bbabadbfaa518803ddc6d5af1dbe9bf874
Emma Ruyle <46852684+Rolliad@users.noreply.github.com>
2021-04-17 18:35:13 -0500

    Add files via upload

---
+++ Initials.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 17 18:30:17 2021
+
+@author: emmar
+"""
+initials = """
+EEEEE RRRR
+E     R   R
+E     R   R
+EEE   RRRR
+E     R R
+E     R  R
+EEEEE R   R
+"""
+print(initials)
