78f2d631a84170644d27f9e06ffc884e32b4d2ad
Emma Ruyle <46852684+Rolliad@users.noreply.github.com>
2021-04-28 15:33:51 -0500

    Add files via upload

---
+++ Magic8Ball.py
@@ -0,0 +1,42 @@
+#libraries to import
+import random
+
+#Name of the person using the 8-ball
+name = "Emma"
+
+#Question asked
+question = "Is this program working right?"
+
+#The Answer given
+answer = 1
+
+#Random number generator for 1-9
+random_number = random.randint(1, 9)
+print(random_number)
+
+#Core logic
+if random_number == 1:
+  answer = "Yes - definitely."
+elif random_number == 2:
+  answer = "It is decidedly so."
+elif random_number == 3:
+  answer = "Without a doubt."
+elif random_number == 4:
+  answer = "Reply hazy, try again."
+elif random_number == 5:
+  answer = "Ask again later."
+elif random_number == 6:
+  answer = "Better not tell you now."
+elif random_number == 7:
+  answer = "My sources say no."
+elif random_number == 8:
+  answer = "Outlook not so good."
+elif random_number == 9:
+  answer = "Very doubtful."
+else:
+  answer = "Error"
+
+#Showing the results
+print(name + " asks: " + question)
+
+print("Magic 8-ball's answer: "+ answer)
